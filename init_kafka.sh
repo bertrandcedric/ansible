f12cbee4687578733dbd5f2f55c7e8ba113e3b6a
CEDRIC BERTRAND <bertrandcedric.cbe@gmail.com>
2016-06-07 16:49:55 +0200

    ajout cassandra + refacto

--- init_kafka.sh
+++ init_kafka.sh
@@ -6,4 +6,4 @@ export NB_MACHINE=2;
 docker-compose -f compose/kafka-compose.yml -p kafka up -d
 docker-compose -f compose/kafka-compose.yml -p kafka scale kafka=2
 
-ansible-playbook ansible/prerequis.yml -i ansible/hosts/kafka/hosts --extra-vars "{\"public_ssh_key\" : \"$(cat ~/.ssh/id_rsa.pub)\"}" -k -c paramiko
+ansible-playbook ansible/prerequis.yml -i ansible/hosts/kafka/hosts --extra-vars "{\"public_ssh_key\" : \"$(cat ~/.ssh/id_rsa.pub)\"}" -k 
