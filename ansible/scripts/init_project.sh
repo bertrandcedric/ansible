db0ca0d21980978dc39345b9856f2be79ae1fb08
Cedric BERTRAND <bertrandcedric.cbe@gmail.com>
2016-09-29 16:18:56 +0200

    mise en place d un host docker dynamique

--- ansible/scripts/init_project.sh
+++ ansible/scripts/init_project.sh
@@ -7,6 +7,6 @@ docker network create ${PROJECT}_default
 
 docker-compose -f compose/${PROJECT}-compose.yml -p ${PROJECT} scale $2
 
-ENV_PROJECT=${PROJECT} ansible-playbook prerequis.yml -i hosts/hosts --extra-vars "{\"public_ssh_key\" : \"$(cat ~/.ssh/id_rsa.pub)\"}" -k
+ENV_PROJECT=${PROJECT} ansible-playbook playbook_prerequis.yml -i hosts/hosts --extra-vars "{\"public_ssh_key\" : \"$(cat ~/.ssh/id_rsa.pub)\"}" -k
 
 ENV_PROJECT=${PROJECT} ansible -m ping -i hosts/hosts all -u deploy
