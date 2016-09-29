4026dcdee3d5aa021e9542d14341ea2e15283b3f
Cedric BERTRAND <bertrandcedric.cbe@gmail.com>
2016-09-29 11:10:07 +0200

    mise en place d un host docker dynamique

--- init_kafka.sh
+++ init_kafka.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
+cd ansible
 
-export ANSIBLE_HOST_KEY_CHECKING=False;
 export ENV_PROJECT=kafka
+
+docker-compose -f compose/${ENV_PROJECT}-compose.yml -p ${ENV_PROJECT} stop
+docker-compose -f compose/${ENV_PROJECT}-compose.yml -p ${ENV_PROJECT} rm
+docker network rm ${ENV_PROJECT}_default
 
 docker network create ${ENV_PROJECT}_default
 docker-compose -f compose/${ENV_PROJECT}-compose.yml -p ${ENV_PROJECT} scale kafka=2 zoo_keeper=1 graphite=1 grafana=1 client=1
 
-ansible-playbook ansible/prerequis.yml -i ansible/hosts/hosts --extra-vars "{\"public_ssh_key\" : \"$(cat ~/.ssh/id_rsa.pub)\"}" -k
+ansible-playbook prerequis.yml -i hosts/hosts --extra-vars "{\"public_ssh_key\" : \"$(cat ~/.ssh/id_rsa.pub)\"}" -k
